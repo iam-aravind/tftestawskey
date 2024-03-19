ce6f3e927ac2527fd2f9c9008cf1c8f65e9ed42b
iam-aravind <94608208+iam-aravind@users.noreply.github.com>
2024-03-19 15:04:52 +0530

    Update keyfile.tf

--- keyfile.tf
+++ keyfile.tf
@@ -9,3 +9,8 @@ output "private_key_pem" {
   value = tls_private_key.rsa.private_key_pem
   sensitive = true
 }
+
+output "private_key_pem" {
+  value = tls_private_key.rsa.public_key_pub
+  sensitive = true
+}
